6a11879dc6125ac04a423f2049abced6b663f6d1
Miguel Cayaban <79849817+Migs29haha@users.noreply.github.com>
2022-07-23 09:51:11 +0800

    Uploaded Octave that prints my fullname

---
+++ Miguel_T_Cayaban.m
@@ -0,0 +1,60 @@
+# Created by Octave 7.1.0, Sat Jul 23 09:49:40 2022 GMT <unknown@DESKTOP-EO34FG6>
+# name: A
+# type: matrix
+# rows: 1
+# columns: 3
+ 1 3 5
+
+
+# name: B
+# type: matrix
+# rows: 1
+# columns: 3
+ 1 1 1
+
+
+# name: Fullname
+# type: sq_string
+# elements: 1
+# length: 20
+MiguelTriasCayaban
+
+
+# name: a
+# type: sq_string
+# elements: 1
+# length: 3
+abc
+
+
+# name: ans
+# type: sq_string
+# elements: 1
+# length: 20
+MiguelTriasCayaban
+
+
+# name: var
+# type: string
+# elements: 1
+# length: 5
+hello
+
+
+# name: var2
+# type: string
+# elements: 1
+# length: 11
+Hello World
+
+
+# name: x
+# type: scalar
+1
+
+
+# name: y
+# type: scalar
+2
+
+
